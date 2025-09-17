8f7f675710e51a789967780f39d64caced75e10d
trooper0x000 <desai.pranav238@gmail.com>
2025-09-17 19:03:13 +0530

    maven project create

---
+++ dyna/src/main/java/com/t0x000/dyna/App.java
@@ -0,0 +1,13 @@
+package com.t0x000.dyna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
